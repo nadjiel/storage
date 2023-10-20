a867dfe97a9d68c5d95ae1c9fab9f1e17c928ee8
Daniel O Sousa <oliveira.daaaaniel@gmail.com>
2023-10-20 10:38:38 -0300

    feat: adding the product repository

---
+++ src/main/java/com/nadjiel/storage/repositories/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.nadjiel.storage.repositories;
+
+import com.nadjiel.storage.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+
+
+}
